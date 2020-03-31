5d4603cf809b82ff59a2b69d5011a207773ad9ff
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-03-31 10:43:55 +0100

    Created whale talk translator according to guidelines.

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-const input = "whale talk";
+const input = "turpentine and turtles";
 const vowels = ["a", "e", "i", "o", "u"];
 const resultArray = [];
+let whaleMessage = "";
 
 for (let i = 0; i < input.length; i++) {
-  console.log(input[i]);
+  //   console.log(input[i]);
+  vowels.forEach(item => {
+    // Whales double their e‘s and the u‘s in their language.
+    if (item === input[i]) {
+      switch (item) {
+        case "e":
+          return resultArray.push(item);
+          break;
+
+        case "u":
+          return resultArray.push(item, item);
+          break;
+
+        case "i":
+          return resultArray.push(item);
+          break;
+
+        case "a":
+          return resultArray.push(item);
+          break;
+
+        case "o":
+          return resultArray.push(item);
+          break;
+      }
+    }
+  });
+  whaleMessage = resultArray.join("").toUpperCase();
 }
+
+whaleMessage;
